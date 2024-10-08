37dd9fd167e75b4eeb071af11627287a4e66a8a8
AlonsoVine <alonvineba@gmail.com>
2024-10-08 12:40:04 +0200

    Proyecto creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { CertificadosComponent } from './certificados/certificados.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PerfilComponent,
+    HabilidadesComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    CertificadosComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
